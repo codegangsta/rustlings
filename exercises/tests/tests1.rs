7a2f22814b4d08cde23a373e5257406c0f52fabf
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-05-17 13:36:08 -0700

    tests

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,13 +6,10 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail!
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(true, "{}", true);
     }
 }
